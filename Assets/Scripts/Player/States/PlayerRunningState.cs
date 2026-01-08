3562aa15c4d824b80e977b122d68a192200a7f0c
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-08 15:45:00 -0300

    Create PlayerRunningState.cs

---
+++ Assets/Scripts/Player/States/PlayerRunningState.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.StateMachine;
+
+public class PlayerRunningState : StateBase
+{
+    private Player _player;
+    public override void OnStateEnter(object o = null)
+    {
+        base.OnStateEnter(o);
+        _player = (Player)o;
+
+    }
+
+    public override void OnStateStay()
+    {
+        base.OnStateStay();
+
+        if (_player.inputAxisVertical == 0)
+        {
+            _player.stateMachine.stateMachine.SwitchState(PlayerStateMachine.PlayerStates.IDLE, _player);
+        }
+
+                if (!_player.characterController.isGrounded)
+        {
+            _player.stateMachine.stateMachine.SwitchState(PlayerStateMachine.PlayerStates.JUMPING, _player);
+        }
+
+    }
+}
