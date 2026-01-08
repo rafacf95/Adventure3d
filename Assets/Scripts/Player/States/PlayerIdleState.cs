2a72f2638c08f3c23a6a93bd6f659f1f7daedde3
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-08 15:44:36 -0300

    Update PlayerIdleState.cs

--- Assets/Scripts/Player/States/PlayerIdleState.cs
+++ Assets/Scripts/Player/States/PlayerIdleState.cs
@@ -5,8 +5,27 @@ using Core.StateMachine;
 
 public class PlayerIdleState : StateBase
 {
+    private Player _player;
     public override void OnStateEnter(object o = null)
     {
         base.OnStateEnter(o);
+        _player = (Player)o;
+
+    }
+
+    public override void OnStateStay()
+    {
+        base.OnStateStay();
+
+        if (_player.inputAxisVertical != 0)
+        {
+            _player.stateMachine.stateMachine.SwitchState(PlayerStateMachine.PlayerStates.RUNNING, _player);
+        }
+
+        if (!_player.characterController.isGrounded)
+        {
+            _player.stateMachine.stateMachine.SwitchState(PlayerStateMachine.PlayerStates.JUMPING, _player);
+        }
+
     }
 }
