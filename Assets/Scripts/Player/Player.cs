e8e0e20ab59be7bb6c4c798c982ff392612a772a
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-08 15:44:20 -0300

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public PlayerStateMachine stateMachine;
     public CharacterController characterController;
     public float speed = 15f;
     public float turnSpeed = 25f;
-    public float gravity = -9.8f;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 15f;
+
+    public float inputAxisVertical;
+
 
     private float _vSpeed = 0f;
 
@@ -15,10 +20,20 @@ public class Player : MonoBehaviour
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
 
-        var inputAxisVertical = Input.GetAxis("Vertical");
-        var speedVector = transform.forward * inputAxisVertical * speed;
+        inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = inputAxisVertical * speed * transform.forward;
 
-        _vSpeed = gravity * Time.deltaTime;
+        if (characterController.isGrounded)
+        {
+            _vSpeed = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _vSpeed = jumpSpeed;
+            }
+        }
+
+        _vSpeed -= gravity * Time.deltaTime;
+        speedVector.y = _vSpeed;
 
         characterController.Move(speedVector * Time.deltaTime);
     }
