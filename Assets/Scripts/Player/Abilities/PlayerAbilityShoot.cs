75c7fb4c17f6bd024f18a7635b5f7f530cd6b80d
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-09 17:31:01 -0300

    Update PlayerAbilityShoot.cs

--- Assets/Scripts/Player/Abilities/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/Abilities/PlayerAbilityShoot.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
+
+    public List<UIGunUpdater> uIGunUpdaters;
+
     public GunBase gunBase;
+    public Transform gunPosition;
+
+    private GunBase _currentGun;
+
     protected override void Init()
     {
         base.Init();
+        CreateGun();
 
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => StopShoot();
     }
+
+    private void CreateGun()
+    {
+        _currentGun = Instantiate(gunBase, gunPosition);
+        _currentGun.uIGunUpdaters = uIGunUpdaters;
+        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+    }
+
     private void StartShoot()
     {
-        gunBase.StartShoot();
+        _currentGun.StartShoot();
         Debug.Log("Start Shoot");
     }
 
     private void StopShoot()
     {
-        gunBase.StopShoot();
+        _currentGun.StopShoot();
         Debug.Log("Stop Shoot");
     }
 }
