1795f946915bfa053118ae25107545f9a376973e
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-14 15:05:34 -0300

    Update FlashColor.cs

--- Assets/Scripts/Animation/FlashColor.cs
+++ Assets/Scripts/Animation/FlashColor.cs
@@ -8,12 +8,18 @@ public class FlashColor : MonoBehaviour
 
     [Header("Setup")]
     public MeshRenderer meshRenderer;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
     public Color color = Color.red;
     public float duration = .2f;
 
     private Color _defaultColor;
     private Tween _currTween;
 
+    void OnValidate()
+    {
+        if (meshRenderer == null) meshRenderer = GetComponent<MeshRenderer>();
+        if (skinnedMeshRenderer == null) skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
     void Start()
     {
         _defaultColor = meshRenderer.material.GetColor("_EmissionColor");
@@ -21,7 +27,13 @@ public class FlashColor : MonoBehaviour
 
     public void Flash()
     {
-        if (!_currTween.IsActive())
+        if (meshRenderer != null && !_currTween.IsActive())
+        {
             _currTween = meshRenderer.material.DOColor(color, "_EmissionColor", duration).SetLoops(2, LoopType.Yoyo);
+        }
+        else if (skinnedMeshRenderer != null && !_currTween.IsActive())
+        {
+            _currTween = skinnedMeshRenderer.material.DOColor(color, "_EmissionColor", duration).SetLoops(2, LoopType.Yoyo);
+        }
     }
 }
