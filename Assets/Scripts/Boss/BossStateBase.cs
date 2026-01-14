7751954b17e188e5740915be77cabbaa110682fd
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-14 15:07:16 -0300

    Create BossStateBase.cs

---
+++ Assets/Scripts/Boss/BossStateBase.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.StateMachine;
+
+namespace Boss
+{
+
+    public class BossStateBase : StateBase
+    {
+        protected BossBase boss;
+
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            boss = (BossBase)objs[0];
+        }
+    }
+
+    public class BossStateInit : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            boss.StartAnimation();
+            Debug.Log("Boss: " + boss);
+        }
+    }
+
+    public class BossStateWalk : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            boss.GoToRandomPoint();
+        }
+    }
+
+}
