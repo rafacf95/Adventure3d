9a9f83cebf2d299cc4925978ed01c5fee86a66b1
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-14 17:34:01 -0300

    Update BossStateBase.cs

--- Assets/Scripts/Boss/BossStateBase.cs
+++ Assets/Scripts/Boss/BossStateBase.cs
@@ -32,7 +32,47 @@ namespace Boss
         public override void OnStateEnter(params object[] objs)
         {
             base.OnStateEnter(objs);
-            boss.GoToRandomPoint();
+            boss.GoToRandomPoint(OnArrive);
+        }
+
+        private void OnArrive()
+        {
+            boss.SwitchState(BossAction.ATTACK);
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+            boss.StopAllCoroutines();
+        }
+
+    }
+
+    public class BossStateAttack : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
+            boss.StartAttack(EndAttacks);
+        }
+
+        private void EndAttacks()
+        {
+            boss.SwitchState(BossAction.WALK);
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+            boss.StopAllCoroutines();
+        }
+    }
+
+    public class BossStateDeath : BossStateBase
+    {
+        public override void OnStateEnter(params object[] objs)
+        {
+            base.OnStateEnter(objs);
         }
     }
 
