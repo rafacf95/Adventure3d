001b95bf760399074a1c845626d872f60b3424a7
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2026-01-20 17:40:41 -0300

    Modified BossSpawnerHelper.cs
    Modified BossSpawnerHelper.cs to allow the use of cinemachine target group

--- Assets/Scripts/Boss/BossSpawnerHelper.cs
+++ Assets/Scripts/Boss/BossSpawnerHelper.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using Boss;
 using UnityEngine;
+using Cinemachine;
 
 public class BossSpawnerHelper : MonoBehaviour
 {
+    [Header("Spawn Setup")]
     [SerializeField] string tagToCompare = "Player";
     [SerializeField] GameObject boss;
     [SerializeField] Transform spawnPoint;
     [SerializeField] List<Transform> waypoints;
 
+    [Header("Camera Setup")]
+    [SerializeField] CinemachineTargetGroup targetGroup;
+    [SerializeField] GameObject bossCamera;
+
     private GameObject _currSpawned;
 
+    private void AddNewTarget(Transform newTarget, float weigth = 1f, float radius = 0)
+    {
+        if (newTarget != null)
+        {
+            targetGroup.AddMember(newTarget, weigth, radius);
+        }
+    }
+
+    private void RemoveTarget(Transform targetToRemove)
+    {
+        if (targetToRemove != null)
+        {
+            targetGroup.RemoveMember(targetToRemove);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -24,6 +45,8 @@ public class BossSpawnerHelper : MonoBehaviour
                 _currSpawned.transform.position = spawnPoint.transform.position;
                 var bossBase = _currSpawned.GetComponent<BossBase>();
                 bossBase.waypoints = waypoints;
+                AddNewTarget(_currSpawned.transform);
+                bossCamera.SetActive(true);
             }
         }
     }
